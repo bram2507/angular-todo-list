c4c8c7c992bc9e08f493cf25af688ec839c88954
Abraham Álvarez Gómez <abrahamalvarezgomez2507@gmail.com>
2024-08-15 23:21:09 +0200

    feat(/src/component/todo-list,todo-list-item): Create function to add items to list
    Add function to add new items
    Add Styles to <li> tag on child component todo-list-item
    Add separates values todo note  and cross  on <div> tag inside <li> tag

--- src/app/components/todo-list-item/todo-list-item-component.ts
+++ src/app/components/todo-list-item/todo-list-item-component.ts
@@ -4,8 +4,28 @@ import { Component, Input, Output } from '@angular/core';
   selector: 'todo-list-item',
   template: `
     @for (item of list; track $index) {
-    <li>{{ item }}</li>
+    <li>
+      <div>
+        {{ item }}
+      </div>
+      <div>X</div>
+    </li>
     }
+  `,
+  styles: `
+  li{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 250px;
+    min-height: 30px;
+    white-space: pre-wrap;
+    padding: 5px;
+    margin-top: 10px;
+    border: 1px solid black;
+    border-radius: 5px;
+  }
+
   `,
   standalone: true,
 })
--- src/app/components/todo-list/todo-list-component.ts
+++ src/app/components/todo-list/todo-list-component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { TodoListItem } from '../todo-list-item/todo-list-item-component';
 
 @Component({
   selector: 'todo-list',
   template: `
     <ul>
+      <section>
+        <div>
+          <textarea [(ngModel)]="item"></textarea>
+        </div>
+        <div>
+          <button (click)="addListItem()">Añadir</button>
+        </div>
+      </section>
       @if (!list.length) {
       <p>No hay ninguna tarea</p>
       } @else {
@@ -12,12 +21,18 @@ import { TodoListItem } from '../todo-list-item/todo-list-item-component';
       }
     </ul>
   `,
-  imports: [TodoListItem],
+  imports: [TodoListItem, FormsModule],
   standalone: true,
 })
 export class TodoList {
   //Properties
   list: string[] = [];
+  item: string = '';
 
   constructor() {}
+
+  addListItem(): void {
+    this.list.push(this.item);
+    this.item = '';
+  }
 }
